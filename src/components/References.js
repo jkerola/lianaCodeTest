06a599cc54cff6bc5ac23c6772703aaf76687598
Janne Kerola <kerola.janne@gmail.com>
2021-04-14 17:11:13 +0300

    Fix references breakpoints on collapse

--- src/components/References.js
+++ src/components/References.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CardDeck, Container } from 'react-bootstrap'
+import { CardDeck, Row, Col, Container } from 'react-bootstrap'
 import Reference from './Reference'
 
 const References = () => {
@@ -7,27 +7,37 @@ const References = () => {
     <Container className='References-container'>
       <h3 className='text-center mb-4'>References</h3>
       <CardDeck className='References-carddeck'>
-        <Reference
-          img='assets/bosch.JPEG'
-          title='Robert Bosch'
-          body='Bosch is a multinational engineering and electronics company.'
-        />
-        <Reference
-          img='assets/bodyshop.JPEG'
-          title='The Body Shop'
-          body='The Body Shop is a global beauty brand with more
-        than 3000 stores in over 60 countries.'
-        />
-        <Reference
-          img='assets/laplandhotels.JPEG'
-          title='Lapland Hotels'
-          body='Lapland Hotels is the largest and most diverse hotel chain in Lapland.'
-        />
-        <Reference
-          img='assets/ikea.JPEG'
-          title='IKEA'
-          body='IKEA is a multinational furniture store.'
-        />
+        <Row className='justify-content-center'>
+          <Col xs='auto' s='auto' md='auto'>
+            <Reference
+              img='assets/bosch.JPEG'
+              title='Robert Bosch'
+              body='Bosch is a multinational engineering and electronics company.'
+            />
+          </Col>
+          <Col xs='auto' s='auto' md='auto'>
+            <Reference
+              img='assets/bodyshop.JPEG'
+              title='The Body Shop'
+              body='The Body Shop is a global beauty brand with more
+            than 3000 stores in over 60 countries.'
+            />
+          </Col>
+          <Col xs='auto' s='auto' md='auto'>
+            <Reference
+              img='assets/laplandhotels.JPEG'
+              title='Lapland Hotels'
+              body='Lapland Hotels is the largest and most diverse hotel chain in Lapland.'
+            />
+          </Col>
+          <Col xs='auto' s='auto' md='auto'>
+            <Reference
+              img='assets/ikea.JPEG'
+              title='IKEA'
+              body='IKEA is a multinational furniture store.'
+            />
+          </Col>
+        </Row>
       </CardDeck>
     </Container>
   )
